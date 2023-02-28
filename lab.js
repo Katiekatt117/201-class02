d0968982db419e751b3be46356d75d881ef27568
Katiekatt117 <123973932+Katiekatt117@users.noreply.github.com>
2023-02-28 12:24:38 -0600

    update

--- lab.js
+++ lab.js
@@ -60,3 +60,55 @@ whome()
 mybaby()
 hobbyzz()
 areazz()
+
+let userAns = "";
+let thisArray = [4, 5, 7, 12, "Hello"];
+
+function printItem(thisItem){
+    for( let x = 0; x < thisItem.length; x++){
+        console.log(thisItem[x]);
+    }
+}
+
+printItem(thisArray);
+
+function thisFunc(){
+
+    userAns = prompt("Please guess a number between 1 and 10! :)");
+
+
+    while(userAns != 5){
+        if(userAns < 5){
+            alert("Your number is too low! Try again!");
+            userAns = prompt("Please guess number again!");
+        }
+        else if(userAns > 5){
+            alert("Your number is too high! Try again!");
+            userAns = prompt("Please guess number again!");
+        }
+    }
+alert("You guessed right!!!");
+}
+
+
+function promptUser(expectedAnswer) {
+    let chancesleft = 4;
+    let answeredCorrectly = false;
+    while (chancesLeft > 0) {
+        const answer = prompt('Please enter your answer:');
+        if(answer === null) {
+            break;
+        }
+        else if (answer === expectedAnswer) {
+            answeredCorrectly = true;
+            break;
+        }
+        chancesLeft--;
+    }
+    if(!answeredCorrectly) {
+        alert('You are out of chances!');
+    }
+}
+
+printItem()
+promptUser()
